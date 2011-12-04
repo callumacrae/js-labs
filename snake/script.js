59f362b79032094153f7c2e12aa449b2c1fcc945
callumacrae <callum@lynxphp.com>
2011-12-04 16:09:55 +0000

    [snake] Made map smaller.

--- snake/script.js
+++ snake/script.js
@@ -14,26 +14,26 @@ document.addEventListener('keydown', function(e) {
 setInterval(function() {
 	if (direction === 37) {
 		// left
-		if (snake.left > 49) {
-			snake.left -= 50;
+		if (snake.left > 19) {
+			snake.left -= 20;
 			snake.style.left = snake.left + 'px';
 		}
 	} else if (direction === 38) {
 		// up
-		if (snake.top > 49) {
-			snake.top -= 50;
+		if (snake.top > 19) {
+			snake.top -= 20;
 			snake.style.top = snake.top + 'px';
 		}
 	} else if (direction === 39) {
 		// right
-		if (snake.left < 750) {
-			snake.left += 50;
+		if (snake.left < 380) {
+			snake.left += 20;
 			snake.style.left = snake.left + 'px';
 		}
 	} else if (direction === 40) {
 		// down
-		if (snake.top < 550) {
-			snake.top += 50;
+		if (snake.top < 380) {
+			snake.top += 20;
 			snake.style.top = snake.top + 'px';
 		}
 	}

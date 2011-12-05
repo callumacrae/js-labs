d57cfb60a84ae1567e8f5ae1d829a61f3415addb
callumacrae <callum@lynxphp.com>
2011-12-05 16:16:07 +0000

    [map] Added a wall.

--- map/script.js
+++ map/script.js
@@ -5,25 +5,25 @@ cursor.top = 0;
 document.addEventListener('keydown', function(e) {
 	if (e.keyCode === 37) {
 		// left
-		if (cursor.left > 49) {
+		if (((cursor.top <= 50 || cursor.top >= 300) || cursor.left !== 250) && cursor.left > 49) {
 			cursor.left -= 50;
 			cursor.style.left = cursor.left + 'px';
 		}
 	} else if (e.keyCode === 38) {
 		// up
-		if (cursor.top > 49) {
+		if ((cursor.top !== 300 || cursor.left !== 200) && cursor.top > 49) {
 			cursor.top -= 50;
 			cursor.style.top = cursor.top + 'px';
 		}
 	} else if (e.keyCode === 39) {
 		// right
-		if (cursor.left < 750) {
+		if (((cursor.top <= 50 || cursor.top >= 300) || cursor.left !== 150) && cursor.left < 750) {
 			cursor.left += 50;
 			cursor.style.left = cursor.left + 'px';
 		}
 	} else if (e.keyCode === 40) {
 		// down
-		if (cursor.top < 550) {
+		if ((cursor.top !== 50 || cursor.left !== 200) && cursor.top < 550) {
 			cursor.top += 50;
 			cursor.style.top = cursor.top + 'px';
 		}

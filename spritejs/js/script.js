875e972944fab40c6f6e95e7042e5273932b8cbf
callumacrae <callum@lynxphp.com>
2011-12-29 23:09:55 +0000

    [spritejs] Renamed sp to monkey.

--- spritejs/js/script.js
+++ spritejs/js/script.js
@@ -4,37 +4,35 @@ var scene = sjs.Scene({w:640, h:480});
 // load the images in parallel. When all the images are
 // ready, the callback function is called.
 scene.loadImages(['images/monkey.png'], function() {
-    var sp = scene.Sprite('images/monkey.png');
-	sp.move(100, 100).update();
+    var monkey = scene.Sprite('images/monkey.png');
+	monkey.move(100, 100).update();
 
-	sp.yv = 15;
-	sp.xv = 0;
+	monkey.yv = 15;
+	monkey.xv = 0;
 	setInterval(function() {
-		if (sp.y > 300 && sp.yv >= 0) {
-			sp.yv = 0;
-		} else if (sp.yv < 15) {
-			sp.yv++;
+		if (monkey.y > 300 && monkey.yv >= 0) {
+			monkey.yv = 0;
+		} else if (monkey.yv < 15) {
+			monkey.yv++;
 		}
 
-		if (sp.xv < 0) {
-			sp.xv++;
-		} else if (sp.xv > 0) {
-			sp.xv--;
+		if (monkey.xv < 0) {
+			monkey.xv++;
+		} else if (monkey.xv > 0) {
+			monkey.xv--;
 		}
 
-		sp.applyVelocity().update();
-
-
+		monkey.applyVelocity().update();
 	}, 20);
 
 
 	document.addEventListener('keydown', function(e) {
 		if (e.keyCode === 37) {
-			sp.xv = -15;
+			monkey.xv = -15;
 		} else if (e.keyCode === 38) {
-			sp.yv = -15;
+			monkey.yv = -15;
 		} else if (e.keyCode === 39) {
-			sp.xv = 15;
+			monkey.xv = 15;
 		} else if (e.keyCode === 40) {
 		} else {
 			return true;
